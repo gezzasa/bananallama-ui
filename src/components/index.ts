9414c4275ab4dbd28aa8277f236693620e78fa6c
gezzasa <gezzasa@gmail.com>
2023-03-25 17:18:44 +0100

    feat(): export button

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,6 @@ export { default as BlInputGroup } from './forms/molecules/BlInputGroup.vue';
 export { default as BlSelectGroup } from './forms/molecules/BlSelectGroup.vue';
 export { default as BlRichText } from './forms/rich-text/RichTextEditor.vue';
 
+export { default as BlButton } from './forms/atoms/BlButton.vue';
+
 export { default as BlGrid } from './ui/BlGrid.vue';
