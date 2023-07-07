03e6a690be96411b03dbb9acc0320e9ae4ac5e5c
gezzasa <gezzasa@gmail.com>
2023-07-07 08:22:43 +0200

    fix(stories): update stories
    - force release

--- src/stories/BlRadio.stories.js
+++ src/stories/BlRadio.stories.js
@@ -1,4 +1,5 @@
 import BlRadio from '../components/forms/molecules/BlRadio.vue';
+import BlGrid from '../components/ui/BlGrid.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 export default {
@@ -10,7 +11,11 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/vue/writing-stories/args
 export const Vanilla = {
   render: () => ({
-    components: { BlRadio },
-    template: '<BlRadio id="radio-1" v-model="radioValue" label="Radio 1 label" name="radio" value="radio-1" /> <BlRadio id="radio-2" v-model="radioValue" label="Radio 2 label" name="radio" value="radio-2" />',
+    components: { BlRadio, BlGrid },
+    template:
+      '<BlGrid>'+
+        '<BlRadio id="radio-1" v-model="radioValue" label="Radio 1 label" name="radio" value="radio-1" />'+
+        '<BlRadio id="radio-2" v-model="radioValue" label="Radio 2 label" name="radio" value="radio-2" />'+
+      '</BlGrid>',
   }),
 };
